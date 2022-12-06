faec8fe4c5de815ae86cbef262c4f9e5382c4934
Luiz Eduardo <l.eduardos.lopes@gmail.com>
2022-12-06 01:24:58 -0300

    ranger de vendas por  meses selecionados

--- src/pages/vendaUser.jsx
+++ src/pages/vendaUser.jsx
@@ -1,25 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {query, where, getDocs, collection} from 'firebase/firestore'
+import {db, auth} from '../firebase'
 import Sidebar from "../layout/Sidebar";
 import Header from "../layout/Header";
 import { useUserAuth } from "../contexts/AuthContext";
 
 import { listarVendas } from "../firebase/controller";
+import moment from "moment";
 
 function VendaUser() {
-
   const { user } = useUserAuth();
-
+  const [mesInicial, setMesInicial] = useState(moment(new Date()).format("MM") -1);
+  const [mesFinal, setMesFinal] = useState("");
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [userId, setUserId] = useState("");
-  const [userVendas, setUserVendas] = useState([])
+  const [userVendas, setUserVendas] = useState([]);
 
-    function vendasUsuario(nome) {
-        listarVendas(user.uid, 'userID', nome).then(rsult => {
-            console.log(rsult);
-            setUserVendas(rsult)
+  function vendasUsuario(nome) {
+    listarVendas(user.uid, "userID", nome).then((rsult) => {
+      console.log(rsult);
+      setUserVendas(rsult);
+    });
+
+  }
+  const months = moment.months() 
+  const range = months.map(mes => {
+   return moment(mes,"MMMM").format("YYYY-MM")
+  })
+  console.log('reload');
+
+  useEffect(()=>{
+    if(mesInicial && mesFinal){
+      const rangeSliced = range.slice(mesInicial,mesFinal)
+      console.log(rangeSliced);
+      if(rangeSliced.length){
+
+      const colecao = query(collection(db,auth.currentUser.uid),where('mes', 'in', rangeSliced ));
+      getDocs(colecao).then(querySnapshot => {
+        let array = []
+        // console.log(querySnapshot.docs);
+        querySnapshot.forEach(doc => {
+          array.push({...doc.data(), id: doc.id})
+
         })
+        console.log(array)
+        setUserVendas(array)
+      })
+    }else{
+      console.log("vazio")
     }
 
+    }
+  },[userId])
 
   return (
     <div id="main" className="flex h-screen overflow-hidden">
@@ -50,26 +82,29 @@ function VendaUser() {
                     type="text"
                     placeholder="userId"
                     className="form-input"
-                    onInput={e => setUserId(e.target.value)}
+                    onInput={(e) => setUserId(e.target.value)}
                   />
-                  <input
-                    type="month"
-                    name=""
-                    id="month_init"
-                    className="form-input"
-                  />
-                  <input
-                    type="month"
-                    name=""
-                    id="month_end"
-                    className="form-input"
-                  />
-                  <button className="btn font-bold btn bg-zinc-300 border-black mx-1" onClick={() => {vendasUsuario(userId)}}>
-                    F
+                  <select className="form-input w-28" onChange={e => setMesInicial(e.target.value)} >
+                    {months.map((mes,index) => <option className="capitalize" value={index}>{mes}</option>)}
+                  </select>
+                  <select className="form-input w-28" onChange={e => setMesFinal(e.target.value)} >
+                    {months.map((mes,index) => <option className="capitalize" value={index+1}>{mes}</option>)}
+                  </select>
+
+                  <button
+                    className="btn font-bold btn bg-zinc-300 border-black mx-1"
+                    onClick={() => {
+                      vendasUsuario(userId);
+                    }}>
+                    {mesInicial}
                   </button>
                 </header>
                 {/* <div>reusslt</div> */}
-              <p>{userVendas.length}</p>
+                {/* <p>
+                  {userVendas.map((cliente) => {
+                    return <span>{cliente.userID}</span>;
+                  })}
+                </p> */}
               </div>
             </div>
           </div>
