4318a224e5fa739b2a239118ebd0e513a8604c5a
luizdeveloperbr <l.eduardos.lopes@gmail.com>
2023-07-13 23:27:53 -0300

    adicionado componentes no layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar"
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <React.Fragment>
-            <header>nav</header>
-            <main>
-                <Outlet />
-            </main>
-            <footer>copyright</footer>
-        </React.Fragment>
+        <div className="flex h-screen overflow-hidden">
+            <Sidebar />
+            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+                <Header />
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </div>
     )
 }
 export default Layout
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import list_routes from './routers'
 import Layout from './layout'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
