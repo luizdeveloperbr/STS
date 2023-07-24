15074c0b87a9f6b7ef1bd0f43b7fa2c3710ba15d
luizdeveloperbr <l.eduardos.lopes@gmail.com>
2023-07-23 23:37:42 -0300

    refatoraçao de handler de use reduce

--- src/contexts/useLayoutContext.jsx
+++ src/contexts/useLayoutContext.jsx
@@ -2,33 +2,20 @@ import React, { createContext, useContext, useReducer } from "react";
 
 const LayoutContext = createContext()
 
-function handlerLayoutChange(state,action){
-switch (action.toggle){
-    case "sidebarOpened": {
-        return {
-            ...state,
-            sidebarOpened: !state.sidebarOpened
-        }
-    };
-    case "sidebarExpanded": {
-        return {
-            ...state,
-            sidebarExpanded: !state.sidebarExpanded
-        }
-    };
-    case "userMenu": {
-        return {
-            ...state,
-            userMenu: !state.userMenu
-        }
-    };
+function handlerLayoutChange(state, action){
 
+    let newValue = Object.defineProperty(
+        state,
+        action.toggle,
+        { value: !state[action.toggle] }
+    )
+
+    return {...newValue}
 }
-} 
 
 export function LayoutProvider({children}) {
 
-    const [layoutState, setLayoutState] = useReducer(handlerLayoutChange,{sidebarOpened: false, sidebarExpanded: false, userMenu: false})
+    const [layoutState, setLayoutState] = useReducer(handlerLayoutChange,{sidebarOpen: false, sidebarExpanded: false, userMenu: false})
     
     return (
         < LayoutContext.Provider value={{layoutState, setLayoutState}}>
