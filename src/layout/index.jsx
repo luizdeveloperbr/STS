f45b7977bfad2b5201110bf9bb08dce4e61dafca
luizdeveloperbr <l.eduardos.lopes@gmail.com>
2023-07-23 23:37:03 -0300

    converter component para compound pattern

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -3,11 +3,16 @@ import Header from "./Header";
 import Sidebar from "./Sidebar"
 import { Outlet } from "react-router-dom";
 import { LayoutProvider } from '../contexts/useLayoutContext'
-const Layout = () => {
+
+const Layout = ({ routers }) => {
     return (
         <LayoutProvider>
             <div className="flex h-screen overflow-hidden">
-                <Sidebar />
+                <Sidebar.Wrapper>               
+                {/* <Sidebar.Logo icon={null}/> */}
+                <Sidebar.MenuList links={routers} />
+                {/* <Sidebar.ExpandButton /> */}
+                </Sidebar.Wrapper>
                 <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
                     <Header />
                     <main>
